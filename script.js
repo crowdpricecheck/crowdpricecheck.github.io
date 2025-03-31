2988ef5f4d2722464ec451953078f6961f379fbe
crowdpricecheck <nguyennamtran2022@gmail.com>
2025-03-31 13:00:56 -0500

    UPDATE

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function display(event) {
-    let arr = [['Acer', '4GB', '64GB', 'ChromeOS', 'B0D33NC672', 'Integrated', '134.99', 'https://www.amazon.com/acer-Gateway-Chromebook-CBO311-1H-C1MX-802-11ac/dp/B0D33NC672/147-9538335-1828615?th=1'], ['Acer', '8GB', '512GB', 'Windows 11 Home', 'B0CP8D4SM2', 'Dedicated', '679.99', 'https://www.amazon.com/i5-13420H-Processor-GeForce-Display-ANV15-51-51H9/dp/B0CP8D4SM2/147-9538335-1828615?th=1']];
+    let arr = [['Acer', '4GB', '64GB', 'ChromeOS', 'B0D33NC672', 'Integrated', '134.99', 'https://www.amazon.com/acer-Gateway-Chromebook-CBO311-1H-C1MX-802-11ac/dp/B0D33NC672/147-9538335-1828615?th=1'], ['Acer', '8GB', '128GB', 'Windows 11 Home', 'B0BS4BP8FB', 'Integrated', '329.99', 'https://www.amazon.com/A315-24P-R7VH-Display-Quad-Core-Processor-Graphics/dp/B0BS4BP8FB/144-2728663-8063855?th=1'], ['Acer', '8GB', '512GB', 'Windows 11 Home', 'B0CP8D4SM2', 'Dedicated', '679.99', 'https://www.amazon.com/i5-13420H-Processor-GeForce-Display-ANV15-51-51H9/dp/B0CP8D4SM2/147-9538335-1828615?th=1'], ['Asus', '16GB', '1TB', 'Windows 11 Home', 'B0CRDCXRK2', 'Dedicated', '1290.04', 'https://www.amazon.com/ASUS-Display-NVIDIA%C2%AE-i7-13650HX-G614JV-AS74/dp/B0CRDCXRK2/144-2728663-8063855?psc=1']];
     let p = "";
     let found = false;
     event.preventDefault();
